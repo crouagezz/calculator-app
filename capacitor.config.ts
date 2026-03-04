a990c059c709e6543d70af6cf3c9c58f3ac8a511
crouagezz <1498783337@qq.com>
2026-03-04 13:51:05 +0800

    修复 Capacitor 配置文件格式

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.calculator',
-  appName: '苹果计算器',
-  webDir: 'dist'
-};
-
-export default config;
